abc28823504b10f3c4770a81599b2f5b23aacd87
Talita Halboth <talitahalboth@gmail.com>
2021-11-27 16:16:25 -0300

    update

--- gruenAnalyseGenerated.py
+++ gruenAnalyseGenerated.py
@@ -228,7 +228,7 @@ def get_gruen(candidates):
     focus_score = get_focus_score(processed_candidates)
     gruen_score = [min(1, max(0, sum(i))) for i in zip(grammaticality_score, redundancy_score, focus_score)]
     ret = []
-    ret=[0] = gruen_score
+    ret[0] = gruen_score
     ret[1] = grammaticality_score
     ret[2] = redundancy_score
     ret[3] = focus_score
