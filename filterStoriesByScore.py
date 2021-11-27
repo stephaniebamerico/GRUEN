9099474a11e18ef7b058792c084bb7a6ce3fcd09
Talita Halboth <talitahalboth@gmail.com>
2021-11-27 17:05:34 -0300

    added new file

--- filterStoriesByScore.py
+++ filterStoriesByScore.py
@@ -1,16 +1,20 @@
 import csv
+import codecs
 with open("datasetWithScore.csv", newline='', encoding="utf8") as csvfile:
      spamreader = csv.DictReader(csvfile)
+     file = codecs.open("storiesWithHighScore", "w", "utf-8")
      for row in spamreader:
         # print(row['fileName'], )
         try:
             gruen_score = float(row['gruen score'])
             story = row['story']
             # print(gruen_score)
+            tmp = '---------------------------------------------------'
             if (gruen_score > 0.4):
-                print('---------------------------------------------------')
-                print(story)
+                file.write(story)
+                file.write(tmp)
         except:
             x = 0
         #  if row[2] != 0:
         #      print(row[1])
+     file.close()
