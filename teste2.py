a31e1481e07d9bb93e9370b80d5c5d9af1198441
Talita <talita.fernandes@vtex.com.br>
2022-01-10 19:36:50 -0300

    udpate teste2.py
    Signed-off-by: Talita <talita.fernandes@vtex.com.br>

--- teste2.py
+++ teste2.py
@@ -273,7 +273,7 @@ if __name__ == "__main__":
      candidates = [
                    #"This is a good example.",
                    "Ela é realmente muito bonita. Ela é bonita.",
-                   "uma bota boto rosa. A bota boto rosa azul verde.",
+                   "bolo lobo. O lobo comeou o bolo",
                    "um teste aqui. um teste.",
                    "aaaaaaaaaa. aaaaaaaa"
                    #"This is a bad example. It is ungrammatical and redundant. Orellana shown red card for throwing grass at Sergio Busquets. Orellana shown red card for throwing grass at Sergio Busquets.",
