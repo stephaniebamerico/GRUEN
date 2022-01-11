6069bcfc37d38296ae2b04551ad3e9d576c46cd8
Talita <talita.fernandes@vtex.com.br>
2022-01-10 22:57:40 -0300

    udpate teste2.py
    Signed-off-by: Talita <talita.fernandes@vtex.com.br>

--- teste2.py
+++ teste2.py
@@ -189,24 +189,24 @@ def get_redundancy_score(all_summary):
             
             longest_common_substring = difflib.SequenceMatcher(None, a, b).find_longest_match(0, len(a), 0, len(b))
             LCS_string_length = longest_common_substring.size
-            if (printRed): print("LCS_string_length",LCS_string_length)
+            if (printRed): print("LCS_string_length",LCS_string_length, 0.8 * min(len(a), len(b))
             if (printRed): print("longest_common_substring",longest_common_substring)
             if LCS_string_length > 0.8 * min(len(a), len(b)):
                 if (printRed): print("penality lcs string length")
                 flag_num += 1
             LCS_word_length = len(a[longest_common_substring[0]: (longest_common_substring[0]+LCS_string_length)].strip().split())
-            if (printRed): print("LCS_word_length", LCS_word_length)
+            if (printRed): print("LCS_word_length", LCS_word_length,  0.8 * min(len(a_split), len(b_split))
             if (printRed): print("LCS_word", a[longest_common_substring[0]: (longest_common_substring[0]+LCS_string_length)].strip().split())
             if LCS_word_length > 0.8 * min(len(a_split), len(b_split)):
                 if (printRed): print("penality lcs word length")
                 flag_num += 1
             edit_distance = editdistance.eval(a, b)
-            if (printRed): print("edit_distance", edit_distance)
+            if (printRed): print("edit_distance", edit_distance, 0.6 * max(len(a), len(b))
             if edit_distance < 0.6 * max(len(a), len(b)):  # Number of modifications from the longer sentence is too small.
                 flag_num += 1
                 if (printRed): print("penality edit distance")
             number_of_common_word = len([x for x in a_split if x in b_split])
-            if (printRed): print("number_of_common_word",number_of_common_word)
+            if (printRed): print("number_of_common_word",number_of_common_word, 0.8 * min(len(a_split), len(b_split))
             if (printRed): print("common_word",[x for x in a_split if x in b_split])
             if number_of_common_word > 0.8 * min(len(a_split), len(b_split)):
                 if (printRed): print("penality number of common words")
